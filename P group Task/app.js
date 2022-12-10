abe88a4d6b6e518e16a231d657f63f28bfbc46a2
Abhilash4597 <abhilash4597@gmail.com>
2022-12-11 00:51:30 +0530

    click game for practicing dom

--- P group Task/app.js
+++ P group Task/app.js
@@ -1,2 +1,19 @@
-const block = document.querySelectorAll("table-col");
-console.log(block)
+const block = document.querySelector("table");
+let arr =[];
+
+const clickBlock = (e)=> {
+    console.log(e);
+    if(e.target.classList.contains("table-col")){
+        if(arr.length>1){
+            let arr1 = arr.shift();
+            arr1.classList.remove("hero-col");
+        }
+        arr.push(e.target);
+    }
+    arr.forEach((el)=>{
+        el.classList.add("hero-col")
+    })
+}
+
+block.addEventListener("click",clickBlock)
+// console.log(block)
